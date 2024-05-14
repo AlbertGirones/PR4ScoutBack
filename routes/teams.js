20ff59d9d101d42314caec3ac47b6673bce72fea
AlbertGirones <agirones600@gmail.com>
2024-05-14 23:08:41 +0200

    Back of photo team

--- routes/teams.js
+++ routes/teams.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const connection = require('../db/connection');
 
 // Obtener todos los equipos
@@ -14,13 +16,32 @@ router.get('/teams', (req, res) => {
   });
 });
 
-// Crear un nuevo equipo
-router.post('/teams', (req, res) => {
-  const { name, category, image } = req.body;
-  // Aquí debes guardar la imagen y obtener su ruta
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Carpeta donde se guardarán las imágenes
+  },
+  filename: (req, file, cb) => {
+    // Generar un nombre de archivo único basado en el ID del equipo y el nombre original del archivo
+    const uniqueFilename = `${req.body.league_id}-${file.originalname}`;
+    cb(null, uniqueFilename); // Utiliza el nombre de archivo único
+  }
+});
+
+const upload = multer({ storage: storage });
+
+// Ruta POST para crear un nuevo equipo
+router.post('/teams', upload.single('image'), (req, res) => {
+  const { name, league_id } = req.body;
+  const image = req.file;
+
+  if (!image) {
+    return res.status(400).json({ error: 'Debes proporcionar una imagen' });
+  }
+
+  const imagePath = '/uploads/' + image.filename;
 
   // Luego inserta el nuevo equipo en la base de datos
-  connection.query('INSERT INTO teams (name, category, image) VALUES (?, ?, ?)', [name, category, image], (error, results) => {
+  connection.query('INSERT INTO teams (name, league, image) VALUES (?, ?, ?)', [name, league_id, imagePath], (error, results) => {
     if (error) {
       console.error('Error al crear el equipo:', error);
       res.status(500).json({ error: 'Error interno del servidor' });
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const userDataRoutes = require('./routes/userDataRoutes');
 const teamsRoutes = require('./routes/teams');
 const leaguesRoutes = require('./routes/leagues');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000', // Reemplaza esta URL con la URL de tu aplicación de React
+  credentials: true // Permite el intercambio de cookies entre el cliente y el servidor
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

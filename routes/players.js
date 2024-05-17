b80b2536f794d7e8814ccc8b396c774fe6f4f697
AlbertGirones <agirones600@gmail.com>
2024-05-17 09:10:47 +0200

    FixAddPlayer

--- routes/players.js
+++ routes/players.js
@@ -10,48 +10,35 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/'); // Carpeta donde se guardarán las imágenes
   },
   filename: (req, file, cb) => {
-    // Generar un nombre de archivo único basado en el ID del equipo y el nombre original del archivo
     const uniqueFilename = `${req.body.league_id}-${file.originalname}`;
-    cb(null, uniqueFilename); // Utiliza el nombre de archivo único
+    cb(null, uniqueFilename);
   }
 });
   
 const upload = multer({ storage: storage });
   
-// Ruta POST para crear un nuevo equipo
+// Ruta POST para agregar jugador //
+
 router.post('/players', upload.single('image'), (req, res) => {
-  const { federationNumber, name, description, team } = req.body;
+  const { federationNumber, name, description, team, position } = req.body;
   const image = req.file;
 
   if (!image) {
       return res.status(400).json({ error: 'Debes proporcionar una imagen' });
   }
 
-  // Crear la ruta de la carpeta de uploads
-  const uploadsFolderPath = path.join(__dirname, '..', 'uploads');
+  const relativeImagePath = path.join('uploads', 'playerUploads', federationNumber, image.filename);
+  console.log(relativeImagePath);
+  const absoluteImagePath = path.join(__dirname, '..',  relativeImagePath);
   
-  // Crear la ruta de la carpeta de playerUploads
-  const playerUploadsFolderPath = path.join(uploadsFolderPath, 'playerUploads');
-  
-  // Verificar si la carpeta de playerUploads ya existe, si no, crearla
+  const playerUploadsFolderPath = path.dirname(absoluteImagePath);
   if (!fs.existsSync(playerUploadsFolderPath)) {
-    fs.mkdirSync(playerUploadsFolderPath);
+    fs.mkdirSync(playerUploadsFolderPath, { recursive: true });
   }
 
-  // Crear la ruta de la carpeta del jugador dentro de playerUploads
-  const playerFolderPath = path.join(playerUploadsFolderPath, federationNumber);
-  
-  // Verificar si la carpeta del jugador ya existe, si no, crearla
-  if (!fs.existsSync(playerFolderPath)) {
-    fs.mkdirSync(playerFolderPath);
-  }
+  fs.renameSync(image.path, absoluteImagePath);
 
-  // Mover la imagen al directorio del jugador
-  const imagePath = path.join(playerFolderPath, image.filename);
-  fs.renameSync(image.path, imagePath);
-
-  // Luego inserta el nuevo equipo en la base de datos
-  connection.query('INSERT INTO players (federationNumber, name, description, team, image) VALUES (?, ?, ?, ?, ?)', [federationNumber, name, description, team, imagePath], (error, results) => {
+  connection.query('INSERT INTO players (federationNumber, name, description, team, position, image) VALUES (?, ?, ?, ?, ?, ?)', [federationNumber, name, description, team, position, relativeImagePath], (error, results) => {
     if (error) {
       console.error('Error al crear el equipo:', error);
       res.status(500).json({ error: 'Error interno del servidor' });
